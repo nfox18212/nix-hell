a5b32df199a72a3058bfc353e4f137a14868c356
nfox18212 <nfox18212@gmail.com>
2025-04-10 00:38:54 -0400

    updating nixos configuation

--- configuration.nix
+++ configuration.nix
@@ -73,7 +73,7 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
@@ -153,6 +153,10 @@
   rustup
   krita
   bat-extras.batman
+  lsb-release
+  kdePackages.kcalc
+  octave
+  ncurses-abi5-compat
   ];
 
   programs.steam = {
